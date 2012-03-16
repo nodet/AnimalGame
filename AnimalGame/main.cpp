e53cd8719d0d704ae15958cb63a064e43728134f
nodet <xavier.nodet@fr.ibm.com>
2012-03-16 21:58:38 +0100

    Create intro function

--- AnimalGame/main.cpp
+++ AnimalGame/main.cpp
@@ -3,15 +3,18 @@
 
 #include <iostream>
 
-const char* intro =
+
+void intro()  {
+    const char* intro =
 "Hi, let's play the animal game. You can quit at any time by typing \"Quit.\"\n\
 \n\
 Think of an animal. \n";
-
+    std::cout << intro << std::endl;
+}
 
 int main() {
 
-    std::cout << intro << std::endl;
+    intro();
 
     return 0;
 }
