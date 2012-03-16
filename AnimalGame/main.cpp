a24d1273cadca522c186c4b6ef254326403b24ee
nodet <xavier.nodet@fr.ibm.com>
2012-03-16 22:05:06 +0100

    First question asked

--- AnimalGame/main.cpp
+++ AnimalGame/main.cpp
@@ -8,13 +8,22 @@ void intro()  {
     const char* intro =
 "Hi, let's play the animal game. You can quit at any time by typing \"Quit.\"\n\
 \n\
-Think of an animal. \n";
+Think of an animal.";
     std::cout << intro << std::endl;
 }
+
+
+
+void ask_question()  {
+    const char* is_your_animal = "Is your animal a ";
+    std::cout << is_your_animal << "cat" << "? ";
+}
+
 
 int main() {
 
     intro();
+    ask_question();
 
     return 0;
 }
