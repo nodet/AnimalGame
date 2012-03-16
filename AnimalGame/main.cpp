baa96a93bc7e834a3ba02c1c87a94843de30a503
nodet <xavier.nodet@fr.ibm.com>
2012-03-16 21:57:13 +0100

    Intro printed

--- AnimalGame/main.cpp
+++ AnimalGame/main.cpp
@@ -1,9 +1,17 @@
 
 
 
+#include <iostream>
+
+const char* intro =
+"Hi, let's play the animal game. You can quit at any time by typing \"Quit.\"\n\
+\n\
+Think of an animal. \n";
 
 
 int main() {
+
+    std::cout << intro << std::endl;
 
     return 0;
 }
