4998acf6d41b75c2528e8f0f1a5ed6656f766340
nodet <xavier.nodet@fr.ibm.com>
2012-03-17 09:53:01 +0100

    Accept 'Quit' as an answer

--- AnimalGame/main.cpp
+++ AnimalGame/main.cpp
@@ -1,4 +1,11 @@
 
+/*
+
+Missing tests:
+Yes -> I win
+
+*/
+
 
 
 #include <iostream>
@@ -13,17 +20,55 @@ Think of an animal.";
 }
 
 
-
 void ask_question()  {
     const char* is_your_animal = "Is your animal a ";
     std::cout << is_your_animal << "cat" << "? ";
 }
 
 
+enum PossibleAnswers {
+    Yes, No, Quit
+};
+
+
+std::string get_string(std::istream& s) {
+    std::string result;
+    char c;
+    while (s.get(c)) {
+        if ('\n' == c) {
+            return result;
+        }
+        result += c;
+    }
+    return result;
+}
+
+PossibleAnswers get_answer() {
+    std::string s = get_string(std::cin);  // TODO
+    if (s.compare("Quit") == 0) {        // TODO
+        return Quit;
+    } else if (s.compare("Yes)") == 0) {
+        return Yes;
+    } else {
+        return No;
+    }
+}
+
+
+void say_goodbye()  {
+    const char* goodbye = "Ok. Bye!";
+    std::cout << goodbye << std::endl;
+}
+
 int main() {
 
     intro();
     ask_question();
+    PossibleAnswers answer = get_answer();
+    if (Quit == answer) {
+        say_goodbye();
+        return 0;
+    }
 
     return 0;
 }
